c4eba0a4d9b5eaa28743a213d3c6fe288d59c8fd
Andrey Velychko <60106215+andriyvelychko@users.noreply.github.com>
2020-05-18 15:44:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function(){
+    $('.menu-toggle').click(function () {
+        $(this).toggleClass('active')
+        $('.menu').slideToggle(400)
+    })
+    //active link
+     $('.header__active-top a').each(function () {
+            var location = window.location.href;
+            var link = this.href;
+            if (location == link) {
+                $(this).addClass('active-menu');
+            }
+        }); 
+
+    $('.vacancies-slider').slick({
+        dots: false,
+        arrows: true,
+        /* fade: true, */
+        /* appendArrows: '.arrows', */
+        nextArrow: '<div class="arrows__text arrows__text-right">след<img class="slider-arrows__right" src="img/arrows-right.svg" alt=""></div>',
+        prevArrow: '<div class="arrows__text arrows__text-left"><img class="slider-arrows__left" src="img/arrows-left.svg" alt="">назад</div>',
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+    //--------------------------------------------------
+    //Плавный скрол
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function () {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+        return false;
+    });
+
+});
